67b0bb312e3d4d56eb2ce26d9ce8e1802ea9d322
Bhanumaan <101312783+Bhanumaan@users.noreply.github.com>
2022-03-31 12:00:23 +0530

    Update User Input.py

--- User Input.py
+++ User Input.py
@@ -12,12 +12,16 @@ txt=("the price of the goods is {}")
 print(txt.format(rate))
 
 #in the curly brackets we can specify how to convert value
+
 trt=("the revised price is {0:.2f} for item number {2:.1f} for {1:.2f} number of  goods,which is a very less price for {1} things")
+
 #print(trt.format(rate))
+
 print(trt.format(rate,count,itemno))
 
 #in curly brackets before ":" we give postion and after ":" converison
 
 txt=("I have a {carname}, it is a {year} model ")
+
 print(txt.format(year=1972,carname="Mustang"))
 
